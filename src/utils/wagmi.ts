5729b3e63c077e173a379ac73f0fcddcd44b3894
sudotx <64108848+sudotx@users.noreply.github.com>
2024-11-19 20:16:03 +0100

    configure chains for testing

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,9 +1,9 @@
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { bscTestnet } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit demo',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  chains: [bscTestnet],
 });
