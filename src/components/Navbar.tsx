2acebb7142539d8cfb04e300e62c1917de230b87
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-11-21 14:22:24 +0100

    ALL ON GOD

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+// import { useNavigate } from "react-router-dom";
 // import { BiMenu } from "react-icons/bi"
 // import { Link } from "react-router-dom"
 // import { co\ }
 
 const Navbar = () => {
+  // const navigate = useNavigate();
+  
   return (
     <div className="fixed top-0 w-full flex justify-between py-2 px-3 md:py-10 md:px-28 backdrop-blur-md z-50">
       <div>
-        <span className="text-primary">By</span><span className="text-white">Forex</span>
-        {/* <Link to="/" className="text-2xl md:text-4xl font-bold">
-        </Link> */}
+        <div className="text-2xl md:text-4xl font-bold">
+          <span className="text-primary">By</span><span className="text-white">Forex</span>
+        </div>
         <p className="text-white text-[8px] md:text-[10px]">Decengtralized Investments</p>
       </div>
       <div className="gap-2 flex">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,18 @@ import { ConnectButton } from "@rainbow-me/rainbowkit"
 // import { useState } from "react"
 import { Packakges } from "../utils/constants"
 import PackageCard from "../components/PackageCard"
+import { useNavigate } from "react-router-dom"
 // import { Link } from "react-router-dom"
 
 const Home = () => {
   // const [active, setActive] = useState(0)
+  const navigate = useNavigate();
+
+  const goDashboard = () => {
+    navigate('/dashboard')
+  }
+
+  
 
   return (
     <div>
@@ -31,7 +39,7 @@ const Home = () => {
             ByForex is a fully decengtralized, web3-based investment platform. The platform will offer users tiered investment packages and income streams through smart contracts, aiming for transparency and automation in managing investment pools and Referral-based incomes. The project emphazises a structured investment approach where users must progress sequentially through the investment packages.
           </p>
           <div className="flex w-full justify-center gap-3 py-3">
-            <Link to="/dashboard" className="text-primary border-2 scale-[1.01] rounded-full px-8 py-2 border-primary">Dashboard</Link>
+            <button onClick={goDashboard} className="text-primary border-2 scale-[1.01] rounded-full px-8 py-2 border-primary">Dashboard</button>
             <ConnectButton />
           </div>
         </div>
