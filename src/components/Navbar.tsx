3a21691836b028e651d825f3036cd300a287f294
sudotx <64108848+sudotx@users.noreply.github.com>
2024-11-25 00:06:48 +0100

    merge register into navbar, if user does not exist

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,70 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useEffect } from "react";
-import { useAccount, useDisconnect } from "wagmi"
+import { useParams } from "react-router-dom";
+import { useAccount, useDisconnect, useReadContract, useWriteContract } from "wagmi";
+import { byForexConfig } from "../../abi";
+
+const isValidAddress = (address: string | undefined): boolean => {
+  return /^0x[a-fA-F0-9]{40}$/.test(address || '');
+};
+
+type UserInfo = [
+  string,    // walletAddress
+  bigint,    // currentPackageLevel
+  bigint,    // totalInvestment  
+  bigint,    // directBusinessVolume
+  bigint,    // directReferralCount
+  string,    // upline
+  bigint,    // totalEarnings
+  boolean,   // isRegistered
+  bigint,    // totalWithdrawal
+  bigint,    // highestPackage
+  bigint,    // totalBalance
+  bigint     // poolEarnings
+]
+
 
 const Navbar = () => {
-  const { isConnected } = useAccount();
+  const { address: referralAddress } = useParams();
+  const { isConnected, address } = useAccount();
   const { disconnect } = useDisconnect();
+
+  const { writeContract } = useWriteContract();
+
+  const handleRegister = (upLineAddress: string) => {
+    writeContract({
+      abi: byForexConfig.abi,
+      address: byForexConfig.address as `0x${string}`,
+      functionName: "registerUser",
+      args: [upLineAddress],
+    });
+  };
+
+  const { data: userInfo } = useReadContract({
+    abi: byForexConfig.abi,
+    address: byForexConfig.address as `0x${string}`,
+    functionName: 'users',
+    args: [address],
+  }) as { data: UserInfo }
+
+  console.log("user wallet address", userInfo[0]);
+
+  useEffect(() => {
+    if (isConnected && referralAddress && isValidAddress(referralAddress) && userInfo[0] === "0x0000000000000000000000000000000000000000") {
+      handleRegister(referralAddress as `0x${string}`);
+    }
+  }, [isConnected, referralAddress]);
 
   useEffect(() => {
     console.log("Wallet connected", isConnected);
   }, []);
-  
+
   return (
     <div className="fixed top-0 w-full flex justify-between py-2 px-3 md:py-10 md:px-28 backdrop-blur-md z-40">
       <div className="my-auto">
         <div className="text-2xl md:text-4xl my-auto font-bold">
           <span className="text-primary">By</span><span className="text-white">Forex</span>
         </div>
-        {/* <p className="text-white text-[8px] md:text-[10px]">Decengtralized Investments</p> */}
       </div>
       <div className="gap-2 flex">
         {
@@ -26,9 +74,6 @@ const Navbar = () => {
             <ConnectButton />
           )
         }
-        {/* <div className="my-auto flex text-primary md:hidden">
-          <BiMenu size={30} />
-        </div> */}
       </div>
     </div>
   )
